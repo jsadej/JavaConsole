99ea731418a1b737f77b05fe15572c37073ee219
jonna <joanna.sadej@gmail.com>
2016-05-05 23:13:58 +0200

    main structure

--- src/com/joanna/Main.java
+++ src/com/joanna/Main.java
@@ -1,8 +1,38 @@
 package com.joanna;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scaner = new Scanner(System.in);
+        System.out.print("enter command");
+        while (scaner.hasNextLine()) {
+            String command = scaner.next();
+            System.out.print(command);
+            if(command.equals("dir")) {
+                System.out.print("$ dir");
+
+            }
+            else  if(command.equals("tree")){
+                System.out.print("$ tree");
+            }
+            else if (command.equals("exit")) {
+                System.out.print("Bye");
+                break;
+
+            }
+            else if (command.isEmpty()) {
+                System.out.print("$");
+                break;
+
+            }
+            else {
+             System.out.print("Unknown command"); }
+
+
+
+        }
+        scaner.close();
     }
 }

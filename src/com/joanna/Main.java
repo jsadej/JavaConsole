5a48ca168391409e87b660284d33f072d55e0c3d
jonna <joanna.sadej@gmail.com>
2016-05-06 15:19:42 +0200

    add  showWorkingDirectory method

--- src/com/joanna/Main.java
+++ src/com/joanna/Main.java
@@ -1,41 +1,42 @@
 package com.joanna;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scaner = new Scanner(System.in);
-        Consola c=new Consola();
+        Consola c = new Consola();
         c.displayPrompt();
 
         while (scaner.hasNextLine()) {
             String command = scaner.next();
-            if(command.equals("dir")) {
-                System.out.print("dir");
+            if (command.equals("dir")) {
+                try {
+                    c.showWorkingDirectory();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 c.displayPrompt();
 
 
-            }
-            else  if(command.equals("tree")){
+            } else if (command.equals("tree")) {
                 System.out.print("tree");
                 c.displayPrompt();
 
-            }
-            else if (command.equals("exit")) {
+            } else if (command.equals("exit")) {
                 System.out.print("Bye");
                 break;
 
-            }
-            else if (scaner.next().equals("")) {
+            } else if (scaner.next().equals("")) {
                 System.out.print("Empty");
                 c.displayPrompt();
-            }
-            else {
-             System.out.print("Unknown command");
+            } else {
+                System.out.print("Unknown command");
                 c.displayPrompt();
             }
-
 
 
         }
